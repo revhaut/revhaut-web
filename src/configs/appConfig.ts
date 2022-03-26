126c94b11a358495770393abf8153848546e03eb
Kufre Godwin <kufy201@gmail.com>
2022-03-26 13:58:16 +0100

    fix env issue

--- src/configs/appConfig.ts
+++ src/configs/appConfig.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const { API_PORT } = process.env;
+export const { PORT } = process.env;
--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,16 @@ import app from '../src/app';
 
 dotenv.config();
 // eslint-disable-next-line import/first
-import { API_PORT } from '../src/configs/appConfig';
+import { PORT } from '../src/configs/appConfig';
 
 const server: http.Server = http.createServer(app);
 
-server.listen(API_PORT);
+server.listen(PORT);
 
 server.on('error', (e: Error) => {
   console.log(`Error starting server${e}`);
 });
 
 server.on('listening', () => {
-  console.log(`Server started http://localhost:${API_PORT} on env ${process.env.NODE_ENV || 'dev'}`);
+  console.log(`Server started http://localhost:${PORT} on env ${process.env.NODE_ENV || 'dev'}`);
 });
