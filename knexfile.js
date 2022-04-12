28ae42a13434b8dc2f634f8e4be412fae47a130e
Kufre Godwin <kufy201@gmail.com>
2022-04-12 17:35:51 +0100

    fix : add ssl to the connection string

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,7 @@ module.exports = {
             database: process.env.DB_DATABASE,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
+            ssl: true,
         },
         pool: {
             min: 2,
