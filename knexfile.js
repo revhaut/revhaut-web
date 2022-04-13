9b34abf05ee5fb348c449e68beeb47b69befda2b
Kufre Godwin <kufy201@gmail.com>
2022-04-13 16:31:41 +0100

    fix: issue with knex file

--- knexfile.js
+++ knexfile.js
@@ -8,17 +8,16 @@ module.exports = {
             database: process.env.DB_DATABASE,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            ssl: true,
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            directory: './src/database/migrations',
-        },
-        seeds: {
-            directory: './src/database/seeds',
+            pool: {
+                min: 2,
+                max: 10,
+            },
+            migrations: {
+                directory: './src/database/migrations',
+            },
+            seeds: {
+                directory: './src/database/seeds',
+            },
         },
     },
 };
